2f87f796d543d8679c12d3a2c8e2522ba66ab756
Israel Garcia <ingarcia@miners.utep.edu>
2020-03-12 00:54:18 -0500

    Sign in, sign up forms added with link routing

--- cool-cats-club/src/app/app.module.ts
+++ cool-cats-club/src/app/app.module.ts
@@ -1,14 +1,29 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: LoginFormComponent
+    },{
+      path: 'signin',
+      component: LoginFormComponent
+  },{
+      path: 'signup',
+      component: SignupFormComponent
+  }])
   ],
   providers: [],
   bootstrap: [AppComponent]
