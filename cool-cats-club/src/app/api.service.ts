2a19588c4ec6e12a5a06d0ee06a60d7cde308387
met2224 <metsang@utexas.edu>
2020-03-16 19:19:54 -0500

    access token vars

--- cool-cats-club/src/app/api.service.ts
+++ cool-cats-club/src/app/api.service.ts
@@ -13,6 +13,10 @@ export class ApiService {
   apiPostUrl: string = 'https://api.imgur.com/3/upload';
 
   apiGetUrl: string = 'https://api.imgur.com/3/account/me/images';
+
+  accessToken1: string = '17f9f5c7c047c939dfefaed4d541bf2c1a23fde4';
+  accessToken2: string = 'd521b80cf990c5833cccf6fa19d7207e109544a9';
+  accessToken3: string = '13380cf881fd6a203c6e31945e53dba5a6b8de82';
   
   public getUsers(){
     return this.httpClient.get(this.usersUrl);
