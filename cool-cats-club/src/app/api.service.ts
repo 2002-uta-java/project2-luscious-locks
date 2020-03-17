3b0e331051a769e59df60402dd45e953f9b83ccf
met2224 <metsang@utexas.edu>
2020-03-17 15:12:52 -0500

    api get and post info

--- cool-cats-club/src/app/api.service.ts
+++ cool-cats-club/src/app/api.service.ts
@@ -14,15 +14,21 @@ export class ApiService {
 
   apiGetUrl: string = 'https://api.imgur.com/3/account/me/images';
 
-  accessToken1: string = '17f9f5c7c047c939dfefaed4d541bf2c1a23fde4';
-  accessToken2: string = 'd521b80cf990c5833cccf6fa19d7207e109544a9';
-  accessToken3: string = '13380cf881fd6a203c6e31945e53dba5a6b8de82';
+  // Bearer token when authorizing getAll
+  accessToken: string = '17f9f5c7c047c939dfefaed4d541bf2c1a23fde4';
+
+  // Client-ID token when authorizing getting or posting an image
+  clientId: string = '546c25a59c58ad7';
   
   public getUsers(){
     return this.httpClient.get(this.usersUrl);
   }
 
   public getImages(){
-    return this.httpClient.get(this.apiGetUrl,{ headers: new HttpHeaders().set('Authorization', this.accessToken1)});
+    return this.httpClient.get(this.apiGetUrl,{ headers: new HttpHeaders().set('Authorization', `Bearer ${this.accessToken}`)});
+  }
+
+  public getImage(imageHash){
+    return this.httpClient.get(`this.apiGetUrl${imageHash}`,{ headers: new HttpHeaders().set('Authorization', `Client-ID ${this.clientId}`)});
   }
 }
