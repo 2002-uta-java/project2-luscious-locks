e01ff33157d3ce18e6aeb34c73732f0b3877854b
Israel Garcia <ingarcia@miners.utep.edu>
2020-03-17 16:19:57 -0500

    Dashboard support added

--- cool-cats-club/src/app/app.module.ts
+++ cool-cats-club/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { UsersComponent } from './components/users/users.component';
+import { ImagesComponent } from './components/images/images.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    NavigationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +30,12 @@ import { SignupFormComponent } from './components/signup-form/signup-form.compon
   },{
       path: 'signup',
       component: SignupFormComponent
+  },{
+      path: 'users',
+      component: UsersComponent
+  },{
+      path: 'images',
+      component: ImagesComponent
   }])
   ],
   providers: [],
