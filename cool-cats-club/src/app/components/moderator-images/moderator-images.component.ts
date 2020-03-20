52ae859b1e961bcfa0a292c252e5e706c2a640c3
Israel Garcia <ingarcia@miners.utep.edu>
2020-03-20 13:35:24 -0500

    Added support for moderator nav-bar

---
+++ cool-cats-club/src/app/components/moderator-images/moderator-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moderator-images',
+  templateUrl: './moderator-images.component.html',
+  styleUrls: ['./moderator-images.component.css']
+})
+export class ModeratorImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
