9feea0292d0613ea0e50068c4a2be0c5898b2c67
met2224 <metsang@utexas.edu>
2020-03-22 01:55:29 -0500

    profile partially connected to db. need user update api endpoint

--- cool-cats-club/src/app/components/profile-info/profile-info.component.ts
+++ cool-cats-club/src/app/components/profile-info/profile-info.component.ts
@@ -10,9 +10,9 @@ import { User } from './user';
 })
 export class ProfileInfoComponent implements OnInit {
 
-  username: string = "israel";
+  username: string;
 
-  password: string = "";
+  password: string;
 
   @Input() newUsername: string = "";
 
@@ -24,13 +24,19 @@ export class ProfileInfoComponent implements OnInit {
 
   isInvalid: boolean = false;
 
+  isEmpty: boolean = false;
+
   constructor(private apiService: ApiService, private userSession: UserSessionService ){ }
 
   ngOnInit(): void {
+    let login = atob(this.userSession.getToken()).split(':');
+    this.username = login[0];
+    this.password = login[1];
   }
 
   public saveChanges() {
     if(this.newUsername || this.newPassword) {
+      this.isEmpty = false;
       this.apiService.getUsers(this.userSession.getToken()).subscribe(
         (data) => {
           this.users = data as User[];
@@ -41,13 +47,15 @@ export class ProfileInfoComponent implements OnInit {
             }
           }
           if(!this.isInvalid) {
-            this.username = this.newUsername;
-            this.password = this.newPassword;
+            this.username = this.newUsername ? this.newUsername : this.username;
+            this.password = this.newPassword ? this.newPassword : this.password;
             this.newUsername = '';
             this.newPassword = '';
           }
         }
       );
+    } else {
+      this.isEmpty = true;
     }
   }
 }
