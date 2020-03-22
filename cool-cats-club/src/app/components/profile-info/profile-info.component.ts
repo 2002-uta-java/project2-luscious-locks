041281ed1c0fd0c6e82d3cc7de13df0caeaee441
met2224 <metsang@utexas.edu>
2020-03-22 14:05:43 -0500

    change login data. needs to change auth data

--- cool-cats-club/src/app/components/profile-info/profile-info.component.ts
+++ cool-cats-club/src/app/components/profile-info/profile-info.component.ts
@@ -37,23 +37,39 @@ export class ProfileInfoComponent implements OnInit {
   public saveChanges() {
     if(this.newUsername || this.newPassword) {
       this.isEmpty = false;
-      this.apiService.getUsers(this.userSession.getToken()).subscribe(
+      this.apiService.loginUser(this.username, this.password).subscribe(
         (data) => {
-          this.users = data as User[];
-          for(let user of this.users) {
-            this.isInvalid = this.newUsername === user.username;
-            if(this.isInvalid) {
-              break;
+          let user = data as User;
+          this.apiService.putLoginOnUser(user.id, this.newUsername, this.newPassword, this.userSession.getToken()).subscribe(
+            (data) => {
+                console.log(data);
+                this.username = this.newUsername ? this.newUsername : this.username;
+                this.password = this.newPassword ? this.newPassword : this.password;
+                this.newUsername = '';
+                this.newPassword = '';
+                console.log(this.username);
+                console.log(this.password);
             }
-          }
-          if(!this.isInvalid) {
-            this.username = this.newUsername ? this.newUsername : this.username;
-            this.password = this.newPassword ? this.newPassword : this.password;
-            this.newUsername = '';
-            this.newPassword = '';
-          }
+          );
         }
       );
+      // this.apiService.getUsers(this.userSession.getToken()).subscribe(
+      //   (data) => {
+      //     this.users = data as User[];
+      //     for(let user of this.users) {
+      //       this.isInvalid = this.newUsername === user.username;
+      //       if(this.isInvalid) {
+      //         break;
+      //       }
+      //     }
+      //     if(!this.isInvalid) {
+      //       this.username = this.newUsername ? this.newUsername : this.username;
+      //       this.password = this.newPassword ? this.newPassword : this.password;
+      //       this.newUsername = '';
+      //       this.newPassword = '';
+      //     }
+      //   }
+      // );
     } else {
       this.isEmpty = true;
     }
